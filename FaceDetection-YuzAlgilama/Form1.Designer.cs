2e62edd28a4c1485c7220fd374caae1b5770f259
Cicekenes <58270544+Cicekenes@users.noreply.github.com>
2021-04-13 00:33:43 +0300

    Yuz-Algilama-Face-Detection

---
+++ FaceDetection-YuzAlgilama/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace FaceDetection_YuzAlgilama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnAlgila = new System.Windows.Forms.Button();
+            this.cmbCihazlar = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnAlgila
+            // 
+            this.BtnAlgila.Location = new System.Drawing.Point(647, 54);
+            this.BtnAlgila.Name = "BtnAlgila";
+            this.BtnAlgila.Size = new System.Drawing.Size(150, 33);
+            this.BtnAlgila.TabIndex = 0;
+            this.BtnAlgila.Text = "Algıla-Detect";
+            this.BtnAlgila.UseVisualStyleBackColor = true;
+            this.BtnAlgila.Click += new System.EventHandler(this.BtnAlgila_Click);
+            // 
+            // cmbCihazlar
+            // 
+            this.cmbCihazlar.FormattingEnabled = true;
+            this.cmbCihazlar.Location = new System.Drawing.Point(85, 6);
+            this.cmbCihazlar.Name = "cmbCihazlar";
+            this.cmbCihazlar.Size = new System.Drawing.Size(637, 32);
+            this.cmbCihazlar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cihaz :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(625, 345);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 411);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCihazlar);
+            this.Controls.Add(this.BtnAlgila);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yüz Algılama - Face Detection";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAlgila;
+        private System.Windows.Forms.ComboBox cmbCihazlar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FaceDetection-YuzAlgilama/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FaceDetection_YuzAlgilama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FilterInfoCollection filter; // Bilgisayarımızdaki cihazların tutulduğu sınıftır.
+        VideoCaptureDevice device; //Webcam başlatma
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice); //Cihazımızda bulunan video kayıt araçları
+            foreach (FilterInfo  device in filter )
+            
+                cmbCihazlar.Items.Add(device.Name); // foreach döngüsü ile cihazımızda kayıtlı olan cihazları comboboxa yazdırma
+            cmbCihazlar.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+        }
+
+
+        private void BtnAlgila_Click(object sender, EventArgs e)
+        {
+            device = new VideoCaptureDevice(filter[cmbCihazlar.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+
+        private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> griResim = new Image<Bgr, byte>(bitmap);
+            Rectangle[] dikdortgen = cascadeClassifier.DetectMultiScale(griResim, 1.2, 1);
+            foreach (Rectangle rectangle in dikdortgen)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Green, 1))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+
+                    }
+                }
+
+            }
+            pictureBox1.Image = bitmap;
+        }
+    }
+}
